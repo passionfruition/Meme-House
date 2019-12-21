d83b8c1d79eea280d9f00673c771009e1f9274dc
Madeleine Prak <madeleineprak@gmail.com>
2019-12-21 13:53:14 -0800

    set up homepage format, rough

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Leaderboard() {
+    return(
+        <div className="leaderboard-wrapper">
+            <h1 className="subtitle">Top Memes</h1>
+            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
+            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
+            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
+        </div>
+    )
+}
+
+export default Leaderboard;
--- src/components/MemeGrid.js
+++ src/components/MemeGrid.js
@@ -1,23 +1,20 @@
-import React from "react";
+import React, { Component } from 'react';
 
-function MemeGrid() {
-    return (
-        <div className="wrapper">
-            {/* {for} */}
-            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
-            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
-            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
-            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
-            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
-            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
-            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
-            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
-            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
-            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
-            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
-            <img src="https://via.placeholder.com/800?text=meme" alt="meme"></img>
-        </div>
-    )
+class MemeGrid extends Component {
+    createGrid = () => {
+        let grid = [];
+        for (let i = 0; i < 24; i++) {
+            grid.push(<img alt="meme" key={i} src="https://via.placeholder.com/800?text=meme"></img>);
+        }
+        return grid;
+    }
+    render() {
+        return (
+            <div className="meme-wrapper">
+                {this.createGrid()}
+            </div>
+        )
+    }
 }
 
 export default MemeGrid;
