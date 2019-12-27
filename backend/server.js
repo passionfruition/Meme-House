df7c9e9240302198e0adb7ca1141a8bdef30d0db
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-12-26 16:39:59 -0800

    changed API_PORT to just PORT

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const Data = require('./data')
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 const router = express.Router();
