4490fdd4146e405d5fe5260a582bf0c504f057e6
Madeleine Prak <madeleineprak@gmail.com>
2020-01-04 22:38:08 -0800

    Add like button on hover for grid and some style

--- src/App.js
+++ src/App.js
@@ -33,7 +33,10 @@ class App extends Component {
       grid.push(
         <div className="meme" key={i}>
           <img alt="meme" src="https://picsum.photos/300/300"></img>
-          <i className="fas fa-heart"></i>
+          <div className="like-button">
+          <a className="button"><i class="fas fa-thumbs-up"></i></a>
+          </div>
+          
         </div>);
     }
     return grid;
@@ -64,7 +67,7 @@ class App extends Component {
   }
 
   displayLikeOnHover = () => {
-
+    this.setState({ hovered: true});
   }
 
   render() {
@@ -73,7 +76,7 @@ class App extends Component {
         <MemeModal attribute={this.state.modal} hideModal={this.hideModal} />
         <Navbar showModal={this.showModal} />
         <Leaderboard displayLeaders={this.displayLeaders}/>
-        <MemeGrid createGrid={this.createGrid} />
+        <MemeGrid createGrid={this.createGrid} hovered={this.state.hovered}/>
       </div>
     );
   }
--- src/components/MemeModal.js
+++ src/components/MemeModal.js
@@ -5,7 +5,7 @@ function MemeModal(props) {
         <div className={"modal " + props.attribute}>
             <div className="modal-background"></div>
             <div className="modal-content has-background-white">
-                <h1>helloooo</h1>
+                <h1>helloooo, test</h1>
             </div>
             <button className="modal-close is-large" onClick={props.hideModal} aria-label="close"></button>
         </div>
