75d6ce1f3bd0015a9b2e1c26da36dfda53a3a13f
Gerson Cabrera <ga.cabrera05@gmail.com>
2020-01-06 21:02:56 -0800

    changed 'message' to 'meme' in create method

--- backend/server.js
+++ backend/server.js
@@ -64,15 +64,15 @@ router.delete('/deleteData', (req, res) => {
 router.post('/putData', (req, res) => {
   let data = new Data();
 
-  const { id, message } = req.body;
+  const { id, meme } = req.body;
 
-  if ((!id && id !== 0) || !message) {
+  if ((!id && id !== 0) || !meme) {
     return res.json({
       success: false,
       error: 'INVALID INPUTS',
     });
   }
-  data.message = message;
+  data.meme = meme;
   data.id = id;
   data.save((err) => {
     if (err) return res.json({ success: false, error: err });
