328992d163232fc96f2d689f4d5cdfa0540b7153
Gerson Cabrera <ga.cabrera05@gmail.com>
2020-01-10 13:08:07 -0800

    added get and put methods on App.js, added practice code in render (bottom of file) for now. nothing is being displayed, but data is being inserted into DB

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 // import { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
@@ -13,10 +14,17 @@ class App extends Component {
     zoomModal: "",
     generatorModal: "",
     hovered: false,
-    memeClicked: "https://picsum.photos/800/400"
+    memeClicked: "https://picsum.photos/800/400",
+    // Initializing state for Meme DB - GC
+    memeArray: [],
+    id: 0,
+    meme: null,
+    idToDelete: null,
   }
 
   componentDidMount() {
+    // Getting Data from DB -GC
+    this.getDataFromDB();
     // Bulma mobile toggle
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
     if ($navbarBurgers.length > 0) {
@@ -30,6 +38,39 @@ class App extends Component {
       });
     }
   }
+
+  //get method that uses backend api to get data from DB - GC
+  getDataFromDB = () => {
+    fetch('/api/getData')
+      .then((data) => data.json())
+      .then((res) => this.setState({ data: res.data })
+      ).then(console.log(this.state.data));
+  };
+
+  //put method that uses our backend api to create new entry/upload into DB - GC
+  putMemeInDB = (memeUpload) => {
+    let currentIds = this.state.data.map((data) => data.id);
+    let idToBeAdded = 0;
+    while (currentIds.includes(idToBeAdded)) {
+      ++idToBeAdded;
+    }
+
+    axios.post('/api/putData', {
+      id: idToBeAdded,
+      meme: memeUpload
+    })
+    .then((result) => {
+     let newMeme = result.data.data 
+      console.log(result.data.data)
+      let tempArray = this.state.data;
+      tempArray.push(newMeme);
+      this.setState({ data: tempArray });
+    })
+    .catch((err) => {
+      if(err) 
+      console.log(err);
+    });
+  };
 
   createGrid = () => {
     let grid = [];
@@ -87,6 +128,29 @@ class App extends Component {
   render() {
     return (
       <div className="wrapper">
+        <div>
+          <ul>
+          {this.state.memeArray.length <= 0
+            ? 'NO MEMES YET'
+            : this.state.memeArray.map((result) => (
+                <li style={{ padding: '10px' }} key={result._id}>
+                  <span style={{ color: 'gray' }}> id: </span> {result.id} <br />
+                  <span style={{ color: 'gray' }}> data: </span> {result.meme}
+                </li>
+              ))}
+          </ul>
+          <div style={{ padding: '10px' }}>
+          <input
+            type="text"
+            onChange={(e) => this.setState({ meme: e.target.value })}
+            placeholder="add something in the database"
+            style={{ width: '200px' }}
+          />
+          <button onClick={() => this.putMemeInDB(this.state.meme)}>
+            ADD
+          </button>
+        </div>
+        </div>
         <MemeModal attribute={this.state.generatorModal} hideModal={this.hideModal} />
         <ZoomModal attribute={this.state.zoomModal} hideModal={this.hideModal} memeClicked={this.state.memeClicked}/>
         <Navbar showModal={this.showModal} />
