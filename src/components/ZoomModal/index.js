f456f548f6bcfc2209064efc03eb03c57bf8e9e1
gerarjon <gerarsuaverdez@gmail.com>
2020-01-14 23:10:47 -0800

    changed modal sizing

--- src/components/ZoomModal/index.js
+++ src/components/ZoomModal/index.js
@@ -6,7 +6,9 @@ function ZoomModal(props) {
         <div className={"zoom-modal modal " + props.attribute}>
             <div className="modal-background"></div>
             <div className="modal-content has-background-white">
-                <img src={props.clickedMemeUrl} alt="meme"></img>
+                <p className="image">
+                    <img src={props.clickedMemeUrl} alt="meme"></img>
+                </p>
                 <div className="like-button">
                     <button data-id={props.clickedMemeId} onClick={() => props.likeMeme()} className="button"><i className="fas fa-thumbs-up"></i></button>
                 </div>
