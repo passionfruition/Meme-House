c98f5ca22b18624d4c0aa7222c34f4f6188a41c9
Gerson Cabrera <ga.cabrera05@gmail.com>
2020-01-18 14:33:27 -0800

    added /

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Route for getting all Memes from the db
+
+app.get('/'), function (req,res) {
+  res.render('index');
+}
+
 app.get('/api/memes', function(req, res) {
   Data.find({}, null, {sort: '-createdAt'}, function(err, memes) {
     if(err) {
