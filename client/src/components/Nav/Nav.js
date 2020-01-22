db7443e15bcdf83ae293e7833f70cc436ad76b30
gerarjon <gerarsuaverdez@gmail.com>
2020-01-22 00:51:55 -0800

    fixed logo

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,6 +5,7 @@ import CreateButton from "../CreateButton";
 import UploadButton from "../UploadButton";
 // import LogInButton from '../LogInButton';
 import ReactTooltip from 'react-tooltip';
+import Logo from '../../2.png'
 
 
 function Nav(props) {
@@ -13,7 +14,7 @@ function Nav(props) {
             <nav className="navbar is-fixed-top is-transparent" role="navigation" aria-label="main navigation">
                 <div className="navbar-brand">
                     <a className="navbar-item" href="/">
-                        <img src="https://images.onlinelabels.com/images/clip-art/pitr/pitr_Home_icon.png" className="logo" alt="logo" width="28" height="28"></img>
+                        <img src={Logo} className="logo" alt="logo" width="28"></img>
                         <h1 className="title app-name">meme house</h1>
                     </a>
                     <button className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasic">
